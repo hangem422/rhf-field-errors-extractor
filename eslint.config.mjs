5122d0b501985158068bdc0f7a7515f25e667ffa
Hant <hangem422@gmail.com>
2025-06-03 12:15:37 +0900

    chore(package): setup for test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
 // @ts-check
 
-import tseslint from 'typescript-eslint';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
@@ -9,4 +10,36 @@ export default tseslint.config(
   },
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          useTabs: false,
+          endOfLine: 'auto',
+        },
+      ],
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // Side effect imports.
+            ['^\\u0000'],
+            // Packages. `react` related packages come first.
+            ['^react', '^@?\\w'],
+            // Parent imports. Put `..` last.
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            // Other relative imports. Put same-folder imports and `.` last.
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+    },
+  },
 );
